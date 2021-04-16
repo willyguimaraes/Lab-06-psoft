816af6abae6ce9f1bd8087cec14e1cfd89488c92
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2021-04-16 11:55:34 -0300

    initial commit

---
+++ Lab06-psoft/src/lab6/Main.java
@@ -0,0 +1,14 @@
+package lab6;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
